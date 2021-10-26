a558ed5b11644a751f852c7bb93110347cba7e59
Alena Buiko <abuiko.ma@gmail.com>
2021-10-26 14:43:36 -0400

    initial commit

---
+++ src/components/userForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class userForm extends Component {
+
+    state = {
+        step: 1,
+        firstName: "",
+        lastName: "",
+        email: "",
+        occupation: "",
+        city: "",
+        bio: ""
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default userForm
